e55a61112712f0898f65301041252fbb73ceacea
Emeka Augustine <cafafanscoders@gamil.com>
2019-08-02 20:31:02 -0400

    Create file structures for the redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Users from './Contents/Users';
+import Posts from './Contents/Posts';
 
-function App() {
-  return (
-    <div>
-      App
-    </div>
-  );
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <Users />
+        <Posts />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Contents/Posts.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Posts extends React.Component {
+
+  render() {
+    return (
+      <div>
+        Posts
+      </div>
+    );
+  }
+}
+
+export default Posts;
